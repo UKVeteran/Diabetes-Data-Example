ae1c0ec1e38100a5243b68550b577235fc464ea8
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2018-12-30 13:16:48 +0000

    Add files via upload

--- diabetes.py
+++ diabetes.py
@@ -74,3 +74,143 @@ svc.fit(X_train_scaled, y_train)
 print("Accuracy on training set: {:.3f}".format(
     svc.score(X_train_scaled, y_train)))
 print("Accuracy on test set: {:.3f}".format(svc.score(X_test_scaled, y_test)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+
+from sklearn.neural_network import MLPClassifier
+
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train, y_train)
+
+print("Accuracy on training set: {:.2f}".format(mlp.score(X_train, y_train)))
+print("Accuracy on test set: {:.2f}".format(mlp.score(X_test, y_test)))
+
+mlp = MLPClassifier(random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+print("Accuracy on training set: {:.3f}".format(
+    mlp.score(X_train_scaled, y_train)))
+print("Accuracy on test set: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
+
+
+mlp = MLPClassifier(max_iter=1000, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+print("Accuracy on training set: {:.3f}".format(
+    mlp.score(X_train_scaled, y_train)))
+print("Accuracy on test set: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
+
+mlp = MLPClassifier(max_iter=1000, alpha=1, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+print("Accuracy on training set: {:.3f}".format(
+    mlp.score(X_train_scaled, y_train)))
+print("Accuracy on test set: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
+
+
+
+
+diabetes_mod = diabetes[(diabetes.BloodPressure != 0) & (diabetes.BMI != 0) & (diabetes.Glucose != 0)]
+
+print(diabetes_mod.shape)
+
+
+feature_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+
+X = diabetes_mod[feature_names]
+y = diabetes_mod.Outcome
+
+
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+models = []
+
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('SVC', SVC()))
+models.append(('LR', LogisticRegression()))
+models.append(('DT', DecisionTreeClassifier()))
+models.append(('GNB', GaussianNB()))
+models.append(('RF', RandomForestClassifier()))
+models.append(('GB', GradientBoostingClassifier()))
+
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = diabetes_mod.Outcome, random_state=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+names = []
+scores = []
+
+for name, model in models:
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    scores.append(accuracy_score(y_test, y_pred))
+    names.append(name)
+
+tr_split = pd.DataFrame({'Name': names, 'Score': scores})
+print(tr_split)
+
+names = []
+scores = []
+
+for name, model in models:
+    
+    kfold = KFold(n_splits=10, random_state=10) 
+    score = cross_val_score(model, X, y, cv=kfold, scoring='accuracy').mean()
+    
+    names.append(name)
+    scores.append(score)
+
+kf_cross_val = pd.DataFrame({'Name': names, 'Score': scores})
+print(kf_cross_val)
+
+
+axis = sns.barplot(x = 'Name', y = 'Score', data = kf_cross_val)
+axis.set(xlabel='Classifier', ylabel='Accuracy')
+
+for p in axis.patches:
+    height = p.get_height()
+    axis.text(p.get_x() + p.get_width()/2, height + 0.005, '{:1.4f}'.format(height), ha="center") 
+    
+plt.show()
+
+
+
